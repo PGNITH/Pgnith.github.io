0dac1b303d7dfe867bbc115a137fe95555ddb91e
PGNITH <198003@nith.ac.in>
2021-11-05 18:26:57 +0530

    add

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"icon":"src/images/PGLOGOPURPLE1.svg","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"e7b34def8b6789d7a90d163152a7aa5b"},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,17 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/About.js")),
+  "component---src-pages-contact-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/Contact.js")),
+  "component---src-pages-footer-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/Footer.js")),
+  "component---src-pages-home-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/Home.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/index.js")),
+  "component---src-pages-projects-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/Projects.js")),
+  "component---src-pages-skills-js": preferDefault(require("/home/premashish/Documents/Web Development/Pgnith.github.io/src/pages/Skills.js"))
+}
+
